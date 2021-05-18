0ac9feea52a12386b63d973d3cd299b5599ead10
Carlinhao <carsilva.ti@gmail.com>
2021-05-18 20:49:26 -0300

    adicionando projeto inicial

---
+++ Desafio 1/crud_pessoa/src/crud-pessoa/AppDataContext/ApplicationDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud_pessoa.AppDataContext
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) 
+            : base(options)
+        {
+        }
+    }
+}
