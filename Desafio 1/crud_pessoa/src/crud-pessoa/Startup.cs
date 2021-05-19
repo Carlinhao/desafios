bff623a9c5b6fd58f94025ac01ef31ff2b360073
Carlinhao <carsilva.ti@gmail.com>
2021-05-18 22:17:06 -0300

    Add injeção de dependência.

--- Desafio 1/crud_pessoa/src/crud-pessoa/Startup.cs
+++ Desafio 1/crud_pessoa/src/crud-pessoa/Startup.cs
@@ -1,6 +1,9 @@
+using crud_pessoa.AppDataContext;
+using crud_pessoa.Configurations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +26,8 @@ namespace crud_pessoa
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("SQL_SERVER")));
+            services.SwaggerServices();
             services.AddControllersWithViews();
         }
 
@@ -41,7 +46,7 @@ namespace crud_pessoa
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.SwaggerConfigure();
             app.UseRouting();
 
             app.UseAuthorization();
