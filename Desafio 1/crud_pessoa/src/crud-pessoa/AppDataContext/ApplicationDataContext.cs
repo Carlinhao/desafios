762cf0edd78c9bbfe3456a1885426c4ae3da4fb9
Carlinhao <carsilva.ti@gmail.com>
2021-05-18 22:16:16 -0300

    Criando contexto de banco de dados

--- Desafio 1/crud_pessoa/src/crud-pessoa/AppDataContext/ApplicationDataContext.cs
+++ Desafio 1/crud_pessoa/src/crud-pessoa/AppDataContext/ApplicationDataContext.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using crud_pessoa.Models.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace crud_pessoa.AppDataContext
 {
     public class ApplicationDataContext : DbContext
     {
+        public DbSet<Pessoa> Pessoas { get; set; }
         public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) 
             : base(options)
         {
+
         }
     }
 }
