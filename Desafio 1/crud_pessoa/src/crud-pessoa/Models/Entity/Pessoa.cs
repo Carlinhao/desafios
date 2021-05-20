578ad18dc10b5268bf708b0107b3f2cb29884ee2
Carlinhao <carsilva.ti@gmail.com>
2021-05-19 22:35:27 -0300

    Adicionando data annotations

--- Desafio 1/crud_pessoa/src/crud-pessoa/Models/Entity/Pessoa.cs
+++ Desafio 1/crud_pessoa/src/crud-pessoa/Models/Entity/Pessoa.cs
@@ -1,4 +1,5 @@
 ﻿using crud_pessoa.VOs;
+using System.ComponentModel.DataAnnotations;
 
 namespace crud_pessoa.Models.Entity
 {
@@ -12,8 +13,15 @@ namespace crud_pessoa.Models.Entity
     public abstract class Entity
     {
         public int Id { get; set; }
-        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A idade é obrigatório", AllowEmptyStrings = false)]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O Cpf do usuário é obrigatório", AllowEmptyStrings = false)]
+        [StringLength(11, MinimumLength = 11)]
         public string Cpf { get; set; }
     }
 }
