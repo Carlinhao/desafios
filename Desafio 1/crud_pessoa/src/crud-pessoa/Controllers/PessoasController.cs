a8a19607ae504165e00e509946b525599fbb5d03
Carlinhao <carsilva.ti@gmail.com>
2021-05-19 22:32:46 -0300

    Criação da controller de pessoas

---
+++ Desafio 1/crud_pessoa/src/crud-pessoa/Controllers/PessoasController.cs
@@ -0,0 +1,151 @@
+﻿using crud_pessoa.AppDataContext;
+using crud_pessoa.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud_pessoa.Controllers
+{
+    //[ApiController]
+    //[Route("api/[controller]")]
+    public class PessoasController : Controller
+    {
+        private readonly ApplicationDataContext _context;
+
+        public PessoasController(ApplicationDataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pessoas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pessoas.ToListAsync());
+        }
+
+        // GET: Pessoas/Details/5
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Idade,Nome,Cpf")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pessoa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Edit/5
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+            return View(pessoa);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Idade,Nome,Cpf")] Pessoa pessoa)
+        {
+            if (id != pessoa.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pessoa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PessoaExists(pessoa.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Delete/5
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return View(pessoa);
+        }
+
+        // POST: Pessoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pessoa = await _context.Pessoas.FindAsync(id);
+            _context.Pessoas.Remove(pessoa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return _context.Pessoas.Any(e => e.Id == id);
+        }
+    }
+}
