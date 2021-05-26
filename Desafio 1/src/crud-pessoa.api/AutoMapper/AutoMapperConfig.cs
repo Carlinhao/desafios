c8f6a93be0bd61d0c365bc8eb496e03f9f914800
Carlinhao <carsilva.ti@gmail.com>
2021-05-26 00:07:15 -0300

    Adicionando as configurações do automapper

---
+++ Desafio 1/src/crud-pessoa.api/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using crud_pessoa.api.Dtos;
+using crud_pessoa.api.Entities;
+
+namespace crud_pessoa.api.AutoMapper
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<EnderecoDto, Endereco>().ReverseMap();
+            CreateMap<ContatoDto, Contato>().ReverseMap();
+
+            CreateMap<PessoaDto, Pessoa>()
+                .ForMember(dest => dest.Contato, from => from.MapFrom(x => x.ContatoDto))
+                .ForMember(dest => dest.Endereco, from => from.MapFrom(x => x.EnderecoDto));            
+
+        }
+    }
+}
