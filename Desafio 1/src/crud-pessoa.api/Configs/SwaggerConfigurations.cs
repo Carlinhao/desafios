208d4c7884dfc135989250ec9605e4fde27bada2
Carlinhao <carsilva.ti@gmail.com>
2021-05-26 00:08:00 -0300

    Criando a classe de configuração do swagger

---
+++ Desafio 1/src/crud-pessoa.api/Configs/SwaggerConfigurations.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace crud_pessoa.api.Configs
+{
+    public static class SwaggerConfigurations
+    {
+        public static void SwaggerServices(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Desafio 1 Zup - Cadastro Pessoa",
+                        Version = "v1",
+                        Description = "Desafios de programação .net core Zup",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Carlos Silva",
+                            Url = new Uri("https://github.com/Carlinhao/desafios/tree/feature/nivel1%2Fdesafio1")
+                        }
+                    });
+            });
+        }
+
+        public static void SwaggerConfigure(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+        }
+    }
+}
