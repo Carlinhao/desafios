21392254a7f8b57c3ee4c979e4d4145a746e89cf
Carlinhao <carsilva.ti@gmail.com>
2021-05-26 23:22:15 -0300

    Adicionando log e validação para capturar mensagens de erro

--- Desafio 1/src/crud-pessoa.api/Services/PessoaService.cs
+++ Desafio 1/src/crud-pessoa.api/Services/PessoaService.cs
@@ -1,9 +1,15 @@
 ﻿using AutoMapper;
+using crud_pessoa.api.Configs.Notifications;
+using crud_pessoa.api.Configs.Validators;
 using crud_pessoa.api.Dtos;
+using crud_pessoa.api.Dtos.Responses;
 using crud_pessoa.api.Entities;
 using crud_pessoa.api.Repositories;
-using System;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace crud_pessoa.api.Services
@@ -11,39 +17,69 @@ namespace crud_pessoa.api.Services
     public class PessoaService : IPessoaService
     {
         private readonly IPessoaRepository _pessoaRepository;
+        private readonly ILogger<PessoaService> _logger;
+        private readonly INotificacaoContext _notificacaoContext;
         private readonly IMapper _mapper;
-        public PessoaService(IPessoaRepository pessoaRepository, IMapper mapper)
+        public PessoaService(IPessoaRepository pessoaRepository,
+                             IMapper mapper,
+                             ILogger<PessoaService> logger,
+                             INotificacaoContext notificacaoContext)
         {
-            _pessoaRepository = pessoaRepository;
+            _pessoaRepository = pessoaRepository;            
             _mapper = mapper;
+            _logger = logger;
+            _notificacaoContext = notificacaoContext;
         }        
 
         public async Task<IEnumerable<PessoaDto>> GetAllAsync(string cpf = "")
         {
+            _logger.LogInformation("Buscar todos", cpf);
             var result = _mapper.Map<IEnumerable<PessoaDto>>(await _pessoaRepository.GetAllAsync(cpf));
 
             return result;
         }
 
-        public async Task<int> InsertAsync(PessoaDto pessoaDto)
+        public async Task<ResultResponse> InsertAsync(PessoaDto pessoaDto)
         {
+            var error = ValidadorResult(new PessoaValidator(), pessoaDto);
+
+            if(error.Errors.Count() > 0)
+            {
+                foreach (var item in error.Errors.Select(x => x.ErrorMessage).ToArray().Distinct())
+                {
+                    _notificacaoContext.AddNotification("PessoaService", item, "InsertAsync");
+                }
+                return new ResultResponse();
+            }
+
+            _logger.LogInformation("Inserir um novo registro: ", pessoaDto);
             var pessoaEntity = _mapper.Map<Pessoa>(pessoaDto);
 
             return await _pessoaRepository.InsertAsync(pessoaEntity);
         }
 
-        public async Task<int> UpdateAsync(PessoaUpdateDto pessoaUpdateDto)
+        public async Task<ResultResponse> UpdateAsync(PessoaUpdateDto pessoaUpdateDto)
         {
+           
+
+            _logger.LogInformation("Atualizando um registro: ", pessoaUpdateDto);
             var pessoaEntity = _mapper.Map<Pessoa>(pessoaUpdateDto);
 
             return await _pessoaRepository.InsertAsync(pessoaEntity);
         }
 
-        public async Task<int> DeleteAsync(int id)
+        public async Task<bool> DeleteAsync(int id)
         {
+            _logger.LogInformation("Deletando registro de id: ", id);
             var result = await _pessoaRepository.DeleteAsync(id);
 
             return result;
+        }
+
+        public ValidationResult ValidadorResult<TV, TE>(TV validacao, TE entidade)
+            where TV : AbstractValidator<TE> where TE : class
+        {
+            return validacao.Validate(entidade);
         }
     }
 }
