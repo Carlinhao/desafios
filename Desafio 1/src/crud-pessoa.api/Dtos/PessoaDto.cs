6fca66023bc0aa396be820d083e199582b45eff0
Carlinhao <carsilva.ti@gmail.com>
2021-05-29 20:37:48 -0300

    Adicionando novas propriesdades ao PessoaDto

--- Desafio 1/src/crud-pessoa.api/Dtos/PessoaDto.cs
+++ Desafio 1/src/crud-pessoa.api/Dtos/PessoaDto.cs
@@ -4,11 +4,14 @@ namespace crud_pessoa.api.Dtos
 {
     public class PessoaDto
     {
-        [JsonPropertyName("nome")]
-        public string Nome { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
 
         [JsonPropertyName("cpf")]
         public string Cpf { get; set; }
+
+        [JsonPropertyName("nome")]
+        public string Nome { get; set; }
 
         [JsonPropertyName("contato")]
         public ContatoDto ContatoDto { get; set; }
