c9537f305e21128d3df6cc9adac7066aef5159d7
Carlinhao <carsilva.ti@gmail.com>
2021-05-29 20:32:43 -0300

    Adicionando os DbSet para contato e endereco e os mapeamentos das tabelas para essas entidades.

--- Desafio 1/src/crud-pessoa.api/DbContextConfig/ApplicationDbContext.cs
+++ Desafio 1/src/crud-pessoa.api/DbContextConfig/ApplicationDbContext.cs
@@ -1,16 +1,31 @@
-﻿using crud_pessoa.api.Entities;
+﻿using crud_pessoa.api.DbContextConfig.MappingEntities;
+using crud_pessoa.api.Entities;
 using Microsoft.EntityFrameworkCore;
-
 
 namespace crud_pessoa.api.DbContextConfig
 {
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Contato> Contato { get; set; }
+        public DbSet<Endereco> Endereco { get; set; }
 
+        
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
             : base(options)
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=crud_pessoa;User ID=SA;Password=DockerSql2017!");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pessoa>(new PessoaMapping().Configure);
+            modelBuilder.Entity<Contato>(new ContatoMapping().Configure);
+            modelBuilder.Entity<Endereco>(new EnderecoMapping().Configure);
         }
     }
 }
