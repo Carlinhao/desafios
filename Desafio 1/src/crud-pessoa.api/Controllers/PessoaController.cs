c7a2d5ddcfd1df2abe809b821afaf026044ebbb3
Carlinhao <carsilva.ti@gmail.com>
2021-05-29 20:25:32 -0300

    Refatorando a controller

--- Desafio 1/src/crud-pessoa.api/Controllers/PessoaController.cs
+++ Desafio 1/src/crud-pessoa.api/Controllers/PessoaController.cs
@@ -3,6 +3,7 @@ using crud_pessoa.api.Dtos;
 using crud_pessoa.api.Dtos.Responses;
 using crud_pessoa.api.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -15,61 +16,71 @@ namespace crud_pessoa.api.Controllers
     {
         private readonly IPessoaService _pessoaService;
         private readonly INotificacaoContext _notificacaoContext;
+        private readonly ILogger<PessoaController> _logger;
 
         public PessoaController(IPessoaService pessoaService,
-                                INotificacaoContext notificacaoContext)
+                                INotificacaoContext notificacaoContext,
+                                ILogger<PessoaController> logger)
         {
             _pessoaService = pessoaService;
             _notificacaoContext = notificacaoContext;
+            _logger = logger;
         }
 
 
         [HttpGet]
         [Route("listar-pessoa")]
-        public async Task<ActionResult<IEnumerable<PessoaDto>>> GetPessoasAsync([FromQuery] Documento documento)
+        public async Task<ActionResult<IEnumerable<InsertPessoaDto>>> GetPessoasAsync([FromQuery] Documento documento)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
+
+            _logger.LogInformation("Listar pessoa: ", documento);
 
             var result = await _pessoaService.GetAllAsync(documento.Cpf);
-
-            if (result == null)
-                return BadRequest();
-
-            return Ok(result);
-        }
-
-        [HttpPost]
-        [Route("cadastrar-pessoa")]
-        public async Task<ActionResult<ResultResponse>> InsertPessoaAsync([FromBody] PessoaDto pessoaDto)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest();
-
-            var result = await _pessoaService.InsertAsync(pessoaDto);
+            _logger.LogInformation("Resultado da pesquisa : ", result);
 
             if (_notificacaoContext.HasNotification())
             {
                 return BadRequest(_notificacaoContext.RetornarNoticacoes());
             }
 
-            if (result == null)
-                return NotFound();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("cadastrar-pessoa")]
+        public async Task<ActionResult<ResultResponse>> InsertPessoaAsync([FromBody] InsertPessoaDto pessoaDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _logger.LogInformation("InsertPessoaAsync pessoa: ", pessoaDto);
+
+            var result = await _pessoaService.InsertAsync(pessoaDto);
+            _logger.LogInformation("Resultado da InsertPessoaAsync : ", result);
+
+            if (_notificacaoContext.HasNotification())
+            {
+                return BadRequest(_notificacaoContext.RetornarNoticacoes());
+            }
 
             return Ok(result);
         }
 
         [HttpPut]
         [Route("atualizar-pessoa")]
-        public async Task<ActionResult<ResultResponse>> InsertPessoaAsync([FromBody] PessoaUpdateDto pessoaUpdateDto)
+        public async Task<ActionResult<ResultResponse>> UpdatePessoaAsync([FromBody] PessoaDto pessoaDto)
         {
             if (!ModelState.IsValid)
                 return BadRequest();
 
-            var result = await _pessoaService.UpdateAsync(pessoaUpdateDto);
+            var result = await _pessoaService.UpdateAsync(pessoaDto);
 
-            if (result == null)
-                return BadRequest();
+            if (_notificacaoContext.HasNotification())
+            {
+                return BadRequest(_notificacaoContext.RetornarNoticacoes());
+            }
 
             return Ok(result);
         }
@@ -81,13 +92,14 @@ namespace crud_pessoa.api.Controllers
             if (!ModelState.IsValid)
                 return BadRequest();
 
-            var result = await _pessoaService.DeleteAsync(id);
+            bool result = await _pessoaService.DeleteAsync(id);
+
+            if (_notificacaoContext.HasNotification())
+            {
+                return BadRequest(_notificacaoContext.RetornarNoticacoes());
+            }
+
             return Ok(result);
         }
-    }
-
-    public class Documento
-    {
-        public string Cpf { get; set; }
     }
 }
