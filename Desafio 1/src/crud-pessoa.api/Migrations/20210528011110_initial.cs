c9bad64808515ed3ab287e8bafc46979052027ae
Carlinhao <carsilva.ti@gmail.com>
2021-05-29 20:52:29 -0300

    adicionando novas migrations

---
+++ Desafio 1/src/crud-pessoa.api/Migrations/20210528011110_initial.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace crud_pessoa.api.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contato",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Telefone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    PessoaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contato", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contato_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Pais = table.Column<string>(nullable: true),
+                    Estado = table.Column<string>(nullable: true),
+                    Cidade = table.Column<string>(nullable: true),
+                    Bairro = table.Column<string>(nullable: true),
+                    Rua = table.Column<string>(nullable: true),
+                    Numero = table.Column<int>(nullable: false),
+                    PessoaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Endereco_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contato_PessoaId",
+                table: "Contato",
+                column: "PessoaId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endereco_PessoaId",
+                table: "Endereco",
+                column: "PessoaId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contato");
+
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+        }
+    }
+}
---
+++ Desafio 1/src/crud-pessoa.api/Migrations/20210529020538_v3.cs
@@ -0,0 +1,266 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace crud_pessoa.api.Migrations
+{
+    public partial class v3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contato_Pessoas_PessoaId",
+                table: "Contato");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Endereco_Pessoas_PessoaId",
+                table: "Endereco");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pessoas",
+                table: "Pessoas");
+
+            migrationBuilder.RenameTable(
+                name: "Pessoas",
+                newName: "Pessoa");
+
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "Pessoa",
+                newName: "Documento");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rua",
+                table: "Endereco",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pais",
+                table: "Endereco",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Numero",
+                table: "Endereco",
+                type: "char(6)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Endereco",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Endereco",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Endereco",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Contato",
+                type: "varchar(11)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contato",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Pessoa",
+                type: "varchar(150)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Documento",
+                table: "Pessoa",
+                type: "varchar(11)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pessoa",
+                table: "Pessoa",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contato_Pessoa_PessoaId",
+                table: "Contato",
+                column: "PessoaId",
+                principalTable: "Pessoa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Endereco_Pessoa_PessoaId",
+                table: "Endereco",
+                column: "PessoaId",
+                principalTable: "Pessoa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contato_Pessoa_PessoaId",
+                table: "Contato");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Endereco_Pessoa_PessoaId",
+                table: "Endereco");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pessoa",
+                table: "Pessoa");
+
+            migrationBuilder.RenameTable(
+                name: "Pessoa",
+                newName: "Pessoas");
+
+            migrationBuilder.RenameColumn(
+                name: "Documento",
+                table: "Pessoas",
+                newName: "Cpf");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rua",
+                table: "Endereco",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Pais",
+                table: "Endereco",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Numero",
+                table: "Endereco",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "char(6)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Estado",
+                table: "Endereco",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cidade",
+                table: "Endereco",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Bairro",
+                table: "Endereco",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Contato",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contato",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Pessoas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(150)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cpf",
+                table: "Pessoas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pessoas",
+                table: "Pessoas",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contato_Pessoas_PessoaId",
+                table: "Contato",
+                column: "PessoaId",
+                principalTable: "Pessoas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Endereco_Pessoas_PessoaId",
+                table: "Endereco",
+                column: "PessoaId",
+                principalTable: "Pessoas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
