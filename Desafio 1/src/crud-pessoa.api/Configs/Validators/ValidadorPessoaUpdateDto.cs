9e8d7e742a425cc00198c9f5a8dd4f21df757491
Carlinhao <carsilva.ti@gmail.com>
2021-05-29 20:49:36 -0300

    adicionando validartor para Dtos

---
+++ Desafio 1/src/crud-pessoa.api/Configs/Validators/ValidadorPessoaUpdateDto.cs
@@ -0,0 +1,45 @@
+﻿using crud_pessoa.api.Dtos;
+using FluentValidation;
+
+namespace crud_pessoa.api.Configs.Validators
+{
+    public class ValidadorPessoaUpdateDto : AbstractValidator<InsertPessoaDto>
+    {
+        private const string NomeErrorMessage = "Nome não pode ser vazio ou nulo";
+        private const string CpfErrorMessage = "Cpf não pode ser vazio ou nulo";
+        private const string CpfNumeroErrorMessage = "Numero Cpf deve possuir 11 dígitos";
+        private const string ContatoDtoErroMessage = "Contato não pode ser vazio ou nulo";
+        private const string EnderecoDtoErroMessage = "Endereço não pode ser vazio ou nulo";
+
+        public ValidadorPessoaUpdateDto()
+        {
+            RuleFor(x => x.Nome)
+                .NotNull()
+                .WithMessage(NomeErrorMessage)
+                .NotEmpty()
+                .WithMessage(NomeErrorMessage);
+
+            RuleFor(x => x.Cpf)
+                .NotNull()
+                .WithMessage(CpfErrorMessage)
+                .NotEmpty()
+                .WithMessage(CpfErrorMessage)
+                .MinimumLength(11)
+                .WithMessage(CpfNumeroErrorMessage)
+                .MaximumLength(11)
+                .WithMessage(CpfNumeroErrorMessage);
+
+            RuleFor(x => x.ContatoDto)
+                .NotNull()
+                .WithMessage(ContatoDtoErroMessage)
+                .NotEmpty()
+                .WithMessage(ContatoDtoErroMessage);
+
+            RuleFor(x => x.EnderecoDto)
+                .NotNull()
+                .WithMessage(EnderecoDtoErroMessage)
+                .NotEmpty()
+                .WithMessage(EnderecoDtoErroMessage);
+        }
+    }
+}
